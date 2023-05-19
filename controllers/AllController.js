c2742435c493cd64f93b6a538d25124ef09bcc69
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-05-19 06:44:53 +0530

    Completed

---
+++ controllers/AllController.js
@@ -0,0 +1,15 @@
+export const Krish = (req,res) => {
+    res.send('Sending message from krish function');
+}
+
+export const Abhi = (req,res) => {
+    res.send('Sending message from abhi function');
+}
+
+export const Swaraj = (req,res) => {
+    res.send('Sending message from swaraj function');
+}
+
+export const Vrushab = (req,res) => {
+    res.send('Sending message from vrushab function');
+}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
-
+import {Krish,Abhi,Swaraj,Vrushab} from './controllers/AllController.js';
 const app = express();
 
-app.get('/krish', function (req,res){res.send('Hello....Krishna')})
+app.get('/krish', Krish);
+app.get('/abhi', Abhi);
+app.get('/swaraj', Swaraj);
+app.get('/vrushab', Vrushab);
+
+
+// app.post();
+// app.patch();
+// app.put();
+// app.delete();
 
 app.listen(3000)
