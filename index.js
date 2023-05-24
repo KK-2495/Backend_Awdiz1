1112462dbdf91e0ab91030f5fac86e7cb475182f
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-05-24 11:18:45 +0530

    update

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import morgan from "morgan";
 
 import {Krish,Abhi,Swaraj,Vrushab} from './controllers/AllController.js';
 import router from './routes/userRoutes.js'
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(morgan('dev'));  // use() func is a middleware
 app.use(express.json());   // parsing raw data.
 app.use('/api/v1', router);
+
+mongoose.connect('mongodb+srv://Krish24:Krish%402495@cluster0.s8xz5ha.mongodb.net/')
+.then(()=>console.log("DB Connected"))
+.catch((err) => console.log(err, "=> err"));
+
 
 // app.get('/krish', Krish);
 // app.get('/abhi', Abhi);
